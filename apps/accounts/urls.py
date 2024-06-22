2b43c912226b47e8ea4b7f6d82586b4fb295be54
samueloliveiraf <samueldiablo73@gmail.com>
2024-06-22 19:05:53 -0300

    add

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 
 from django.contrib.auth import views as auth_views
 
-from .views import register_user, register_account
+from .views import register_user, register_account, users_list, toggle_user_status
 
 
 urlpatterns = [
     path('register-user/', register_user, name='register_user'),
     path('register-account/', register_account, name='register_account'),
+
+    path('users/', users_list, name='users_list'),
+    path('toggle_user_status/<str:user_id>/', toggle_user_status, name='toggle_user_status'),
 
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+
 from .forms import AccountCreationForm, UserCreationForm
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .models import Account, User
 
 
 def register_account(request):
@@ -22,3 +27,28 @@ def register_user(request):
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
+
+
+def users_list(request):
+    user = Account.objects.get(id=request.user.id)
+    users = User.objects.filter(account=user)
+
+    users_all = list()
+
+    for user_actual in users:
+        user = Account.objects.get(id=user_actual.user.id)
+        users_all.append(user)
+
+    return render(request, 'users_list.html', {'users': users_all})
+
+
+@login_required
+def toggle_user_status(request, user_id):
+    user = Account.objects.get(id=user_id)
+
+    if request.method == "POST":
+        user.is_active = not user.is_active
+        user.save()
+        return JsonResponse({'status': 'success', 'is_active': user.is_active})
+
+    return JsonResponse({'status': 'failed'})
